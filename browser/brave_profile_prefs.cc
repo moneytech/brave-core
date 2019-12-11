b4d3683fdc596268136efe7042c422dbe4a0b0a7
Andrius Aucinas <aaucinas@brave.com>
2019-12-11 11:34:21 +0000

    puts bandwidth savings predictor behind a build flag

--- browser/brave_profile_prefs.cc
+++ browser/brave_profile_prefs.cc
@@ -9,7 +9,7 @@
 #include "brave/common/brave_wallet_constants.h"
 #include "brave/common/pref_names.h"
 #include "brave/components/brave_shields/browser/brave_shields_web_contents_observer.h"
-#include "brave/components/brave_savings/browser/perf_predictor_web_contents_observer.h"
+#include "brave/components/brave_savings/browser/buildflags/buildflags.h"
 #include "brave/components/brave_sync/brave_sync_prefs.h"
 #include "brave/components/brave_wallet/browser/buildflags/buildflags.h"
 #include "brave/components/brave_wayback_machine/buildflags.h"
@@ -52,6 +52,10 @@
 #include "brave/browser/brave_wallet/brave_wallet_utils.h"
 #endif
 
+#if BUILDFLAG(ENABLE_BRAVE_PERF_PREDICTOR)
+#include "brave/components/brave_savings/browser/perf_predictor_web_contents_observer.h"
+#endif
+
 using extensions::FeatureSwitch;
 
 namespace brave {
@@ -72,8 +76,11 @@ void RegisterProfilePrefsForMigration(
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   brave_shields::BraveShieldsWebContentsObserver::RegisterProfilePrefs(
       registry);
+
+#if BUILDFLAG(ENABLE_BRAVE_PERF_PREDICTOR)
   brave_perf_predictor::PerfPredictorWebContentsObserver::RegisterProfilePrefs(
       registry);
+#endif
 
   // appearance
   registry->RegisterBooleanPref(kLocationBarIsWide, false);
