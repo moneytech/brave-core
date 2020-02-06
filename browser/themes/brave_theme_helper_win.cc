0a4b2c203a19ff64fb2c8bbb92152d18158d0873
mkarolin <max@brave.com>
2020-02-05 20:26:20 -0500

    theme_service_win -> theme_helper_win.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/1160e52886b813ebd72d57f66613f6819abe1344
    
    commit 1160e52886b813ebd72d57f66613f6819abe1344
    Author: Alan Cutter <alancutter@chromium.org>
    Date:   Thu Jan 30 04:10:02 2020 +0000
    
        Decouple browser theme code from currently installed theme object
    
        This CL removes all references to ThemeService::theme_supplier_ when
        computing colors/images by moving the logic up into separate
        ThemeServiceBase class.
    
        Previously, to return a default theme value,
        ThemeService::BrowserThemeProvider used a helper class called
        DefaultScope that would swap ThemeService::theme_supplier_ with nullptr
        while it was in scope to simulate the user having no theme installed in
        order to compute default color values.
        This CL cleans up this hack by making theme_supplier an explicitly
        passed in parameter to the theme logic which has been pulled out into a
        separate ThemeHelper utility for the ThemeService such that it does
        not depend on the active theme_supplier_.
    
        This is a pure refactor with no changes to existing behaviour.
    
        This is in preparation to allow BrowserFrame Widgets to use their own
        CustomThemeSuppliers:
        https://chromium-review.googlesource.com/c/chromium/src/+/2003156
    
        Bug: 1020050

--- browser/themes/brave_theme_helper_win.cc
+++ browser/themes/brave_theme_helper_win.cc
@@ -2,15 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/browser/themes/brave_theme_service_win.h"
+#include "brave/browser/themes/brave_theme_helper_win.h"
 
 #include "chrome/browser/themes/theme_properties.h"
 
-SkColor BraveThemeServiceWin::GetDefaultColor(int id, bool incognito) const {
+SkColor BraveThemeHelperWin::GetDefaultColor(
+    int id,
+    bool incognito,
+    const CustomThemeSupplier* theme_supplier) const {
   // Prevent dcheck in chrome/browser/themes/theme_properties.cc(384)
   // It assumes this id handled in theme service.
-  if (DwmColorsAllowed() && id == ThemeProperties::COLOR_ACCENT_BORDER)
+  if (DwmColorsAllowed(theme_supplier) &&
+      id == ThemeProperties::COLOR_ACCENT_BORDER)
     return dwm_accent_border_color_;
-  // Skip ThemeServiceWin::GetDefaultColor() to prevent using dwm frame color.
+  // Skip ThemeHelperWin::GetDefaultColor() to prevent using dwm frame color.
   return BraveThemeService::GetDefaultColor(id, incognito);
 }
---
+++ chromium_src/chrome/browser/themes/theme_helper_win.h
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "brave/browser/themes/brave_theme_helper.h"
+
+#undef ThemeHelper
+#define ThemeHelper BraveThemeHelper
+
+#include "../../../../../chrome/browser/themes/theme_helper_win.h"
+
+#undef ThemeHelper
+#define ThemeHelper ThemeHelper
